1dcfbcbfe4b4fced2453509f0fd94c297902ff34
Cameron Mozie <camsw0rld14@gmail.com>
2013-10-02 22:28:56 -0400

    Cleaned up code and setup Toast alert for data stored in local storage on search of api

--- Java2Week1/src/com/cmozie/java2week1/MainActivity.java
+++ Java2Week1/src/com/cmozie/java2week1/MainActivity.java
@@ -1,7 +1,6 @@
 package com.cmozie.java2week1;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -44,13 +43,15 @@ public class MainActivity extends Activity {
 
 	//--public statics
 	public static Context _context;
-	//public static LocationDisplay _locationDetails;
+
 	public static Button searchButton;
+	
 	static Spinner spinner = null;
+	
 	public int selected; 
+	
 	FileStuff m_file;
 	
-	public int Sanfran = 0;
 	//layout
 	TextView _popularZips;
 	Button _pop;
@@ -58,9 +59,7 @@ public class MainActivity extends Activity {
 	//bool
 	Boolean _connected = false;
 	
-	//class declarations
-	//FavDisplay _favorites;
-	
+
 	//strings
 	String _zipcode;
 	String _areaCode;
@@ -87,22 +86,12 @@ public class MainActivity extends Activity {
 	String _timezone2;
 	
 	
-	HashMap<String, String> _history;
-	
 	
 	ArrayList<String>_stacks = new ArrayList<String>();
-	
-	
-	
-	/* (non-Javadoc)
-	 * @see android.app.Activity#onCreate(android.os.Bundle)
-	 */
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-
 		
 		//inflating my form.xml
 		View view;
@@ -111,20 +100,8 @@ public class MainActivity extends Activity {
 		
 		//setting contentView to my inflated view/form
 		setContentView(view);
-		
-		
-		
-		
-		
+
 		_context = this;
-		//_popularZips = new TextView(this);
-		
-		//sets _history to the get history call
-		//_history = getHistory();
-	
-		 //logs the _history text if inside local storage
-		//Log.i("HISTORY READ", _history.toString());
-	 
 		
 		 //webConnection jar file usage
 		 _connected = WebStuff.getConnectionStatus(_context);
@@ -134,13 +111,14 @@ public class MainActivity extends Activity {
 			//if no connection
 		}else if(!_connected) {
 			
+			
 			//alert for connection
 			AlertDialog.Builder alert = new AlertDialog.Builder(_context);
 			alert.setTitle("Connection Required!");
 			alert.setMessage("You need to connect to an internet service!");
 			alert.setCancelable(false);
 			alert.setPositiveButton("Alright", new DialogInterface.OnClickListener() {
-				
+			
 				@Override
 				public void onClick(DialogInterface dialog, int which) {
 
@@ -150,8 +128,6 @@ public class MainActivity extends Activity {
 			alert.show();
 		
 		}
-		
-		
 		 
 		 ArrayAdapter<Cities> listAdapter = new ArrayAdapter<Cities>(_context, android.R.layout.simple_spinner_item, new Cities[]{
 				 
@@ -165,21 +141,7 @@ public class MainActivity extends Activity {
 		
 		 spinner = (Spinner) findViewById(R.id.favList);
 		spinner.setAdapter(listAdapter);
-		 //((Spinner) findViewById(R.id.favList)).setAdapter(listAdapter);
-
-		
-
-		//adding items to the spinner
-			//san francisco
-			/*_stacks.add("94105");
-			//Miami
-			_stacks.add("33133");
-			//washington dc
-			_stacks.add("20001");
-			//time square
-			_stacks.add("10036");
-			//Chicago
-			_stacks.add("60106");*/
+	
 		 //popular zipcodes onclick
 		 _pop = (Button) findViewById(R.id.popularZipcodes);
 		 _pop.setOnClickListener(new OnClickListener() {
@@ -201,21 +163,14 @@ public class MainActivity extends Activity {
 		 				}
 		 			});
 		 			alert.show();
-		 			//adds the _favorites/spinner to the view button is clicked
-		 			//Spinner spinnerObj = (Spinner) findViewById(R.id.favList);
-		 			
+
 		 			
 		 			spinner.setVisibility(View.VISIBLE);
 		 			spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
 		 				
 		 				
-		 				
-
-		 				
 						public void onItemSelected(AdapterView<?> parent,View v,int pos, long id){
-		 					
-		 					//String selected = (String) parent.getItemAtPosition(pos).toString();
-		 					
+		 							 					
 		 					String zipcode = "";
 		 					
 		 					int position = spinner.getSelectedItemPosition();
@@ -288,8 +243,7 @@ public class MainActivity extends Activity {
 											_county2 = two.getString("county");
 											_state2 = two.getString("state");
 										
-											//_csa_name2 = two.getString("csa_name");
-											//_cbsa_name2 = two.getString("cbsa_name");
+
 											_latitude2 = two.getString("latitude");
 											_longitude2 = two.getString("longitude");
 											_region2 = two.getString("region");
@@ -307,7 +261,7 @@ public class MainActivity extends Activity {
 											locationInfo(_zipcode, _areaCode, _city, _county, _state, _latitude, _longitude, _csa_name, _cbsa_name, _region, _timezone);
 											locationInfo2(_zipcode2, _area_code2, _city2, _county2, _state2, _latitude2, _longitude2, _csa_name2, _cbsa_name2, _region2, _timezone2);
 											//Trying to read the file stored.
-											Toast toast = Toast.makeText(getBaseContext(),"Read -->" + FileStuff.readStringFile(_context, "temp", false), Toast.LENGTH_SHORT);
+											Toast toast = Toast.makeText(getBaseContext(),"Stored In local Storage!  " + FileStuff.readStringFile(_context, "temp", false), Toast.LENGTH_SHORT);
 											
 											toast.show();
 											
@@ -317,6 +271,7 @@ public class MainActivity extends Activity {
 										} catch (Exception e) {
 											// TODO: handle exception
 											//Alert for any error in entering into textfield if not a zipcode
+											
 											AlertDialog.Builder alert = new AlertDialog.Builder(_context);
 											alert.setTitle("Error");
 											alert.setMessage("There was an error searching for your request. Check connections or make sure zipcode is correct. USA zipcodes only.");
@@ -333,7 +288,6 @@ public class MainActivity extends Activity {
 											Log.e("Handle Message", e.getMessage().toString());
 										}
 										
-				
 										
 									}						
 									
@@ -347,11 +301,8 @@ public class MainActivity extends Activity {
 							startZipcodeIntent.putExtra(ZipcodeService.MESSENGER_KEY, zipcodeMessenger);
 							startZipcodeIntent.putExtra(ZipcodeService.enteredZipcode,zipcode);
 							startService(startZipcodeIntent);
-		 					//trying to call this function and pass in the selectedItemAtPosition string to the function to run the api query on the selected zipcode in the spinner.
-		 					//getLookup(selected);
 		 					
 		 				}
-		 				
 		 			
 		 				public void onNothingSelected(AdapterView<?>parent){
 		 					Log.i("Aborted", "None Selected");
@@ -362,16 +313,13 @@ public class MainActivity extends Activity {
 		 			});
 		 			
 		 			//sets button to non clickable once clicked once 
-		 			_pop.setClickable(false);
-		 			
+		 			_pop.setClickable(false);	
 		 			
 		 		}
 		 			
 		 	});
 
-
 		 _pop.setText("Click here for popular zipcodes");
-
 
 		 }
 	
@@ -430,185 +378,5 @@ public void locationInfo2(String zipcode,String area_code, String city, String c
 	}
 	
 	
-	
-	//get lookup function that allows me to query the api on on click of the search --see searchButton.OnClick 
-	/**
-	 * Gets the lookup.
-	 *
-	 * @param zipcode the zipcode
-	 * @return the lookup
-	 */
-	/*public void getLookup(String zipcode){
-		
-		//this is the base url of the api
-		String baseURL = "http://zipfeeder.us/zip?";
-		
-		//key needed to use api
-		String key = "key=EN4GbNMq";
-		//this empty string accepts an empty string which will be for zipcodes entered
-		String qs = "";
-		try{
-			//allows the empty string to recieve the zipcode string encoded
-			qs = URLEncoder.encode(zipcode, "UTF-8");
-		}catch (Exception e) {
-			
-			//if an error in the api show the bad url alert
-			AlertDialog.Builder alert = new AlertDialog.Builder(_context);
-			alert.setTitle("Bad URL");
-			alert.setMessage("An error occured the in the construction of the URL.Encoding problem.");
-			alert.setCancelable(false);
-			alert.setPositiveButton("Sorry", new DialogInterface.OnClickListener() {
-				
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-
-					dialog.cancel();
-				}
-			});
-			alert.show();
-			Log.e("Bad URL","Encoding Problem");
-			qs = "";
-		}
-		
-		//creates finalURL as a URL
-		URL finalURL;
-		try{
-			//sets the final url to the base plus the api key with the string parameter needed for search as well as the empty string that recieves a zipcode.
-			finalURL = new URL (baseURL + key + "&zips=" + qs);
-			
-			//logs the final url query
-			Log.i("URL",finalURL.toString());
-			
-			mainZipRequest qr = new mainZipRequest();
-			qr.execute(finalURL);
-		}catch (MalformedURLException e){
-			Log.e("BAD URL", "Malformed URL");
-			finalURL = null;
-		}
-	}
-	*/
-	/**
-	 * The Class mainZipRequest.
-	 */
-	/*private class mainZipRequest extends AsyncTask<URL, Void, String>{
-		
-		 (non-Javadoc)
-		 * @see android.os.AsyncTask#doInBackground(Params[])
-		 
-		@Override
-		protected String doInBackground(URL...urls){
-			String response = "";
-		for (URL url : urls) {
-			response = WebStuff.getURLStringResponse(url);
-		}
-			return response;
-		}*/
-		
-		/* (non-Javadoc)
-		 * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
-		 */
-		/*@Override
-		protected void onPostExecute(String result){
-			
-			Log.i("URL RESPONSE", result);
-			try {
-				
-				//creates a json object and json array to access my json objects
-				JSONObject json = new JSONObject(result);
-				JSONArray ja = json.getJSONArray("zips");
-				
-				
-				Log.i("results",result);
-				
-			
-				 
-					//loops through json array 
-					for (int i = 0; i < ja.length(); i++) {
-						//sets a json object to access object values inside array
-						JSONObject one = ja.getJSONObject(i);
-						
-					//setting my text to the values to the strings of the json data
-					_zipcode = one.getString("zip_code");
-					_areaCode = one.getString("area_code");
-					_city = one.getString("city");
-					_state = one.getString("state");
-					_county = one.getString("county");
-					_csa_name = one.getString("csa_name");
-					_cbsa_name = one.getString("cbsa_name");
-					_latitude = one.getString("latitude");
-					_longitude = one.getString("longitude");
-					_region = one.getString("region");
-					_timezone = one.getString("time_zone");
-						 
-					}
-					Log.i("one", _areaCode + _city + _state + _county + _csa_name + _cbsa_name + _latitude + _longitude + _region + _timezone);
-				
-					//sets the values of the text by calling the locationInfo function inside of my Locationdisplay class
-					locationInfo(_areaCode, _city, _county, _state, _latitude, _longitude, _csa_name, _cbsa_name, _region, _timezone);  
-				
-					//confims zipcode is valie
-					Toast toast = Toast.makeText(_context, "Valid Zipcode " + _zipcode , Toast.LENGTH_SHORT);
-					toast.show();
-					
-		
-					
-					
-
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				//Alert for any error in entering into textfield if not a zipcode
-				AlertDialog.Builder alert = new AlertDialog.Builder(_context);
-				alert.setTitle("Error");
-				alert.setMessage("There was an error searching for your request. Check connections or make sure zipcode is correct. USA zipcodes only.");
-				alert.setCancelable(false);
-				alert.setPositiveButton("Alright", new DialogInterface.OnClickListener() {
-					
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-
-						dialog.cancel();
-					}
-				});
-				alert.show();
-				e.printStackTrace();
-				Log.e("JSON","JSON OBJECT EXCEPTION");
-			}
-		}
-		
-	}*/
-		/*@SuppressWarnings("unchecked")
-		private HashMap<String, String> getHistory(){
-			
-			//creates an object named stored that reads the object that is stored in local storage
-			Object stored = FileStuff.readStringFile(_context, "temp", false);
-			
-			//declares the hashmap history variable
-			HashMap<String, String> history;
-			
-			//if theres an error fire alert
-			if (stored == null) {
-				Log.i("HISTORY","NO HISTORY FILE FOUND");
-				AlertDialog.Builder alert = new AlertDialog.Builder(_context);
-				alert.setTitle("Saved Files");
-				alert.setMessage("There are no saved zipcodes in local storage. Once a search is made the zipcode will be saved.");
-				alert.setCancelable(false);
-				alert.setPositiveButton("Alright", new DialogInterface.OnClickListener() {
-					
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-
-						dialog.cancel();
-					}
-				});
-				alert.show();
-				history = new HashMap<String, String>();
-				
-				//else store it into the history
-			}	else {
-				history = (HashMap<String, String>) stored;
-			}
-			return history;
-			
-		}*/
 	
 }
