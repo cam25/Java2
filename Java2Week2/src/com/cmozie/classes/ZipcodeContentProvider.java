997879d3af63b318737675e53c2e8a390e5fe864
Cameron Mozie <camsw0rld14@gmail.com>
2013-10-07 12:33:48 -0400

    Created content provider class

---
+++ Java2Week2/src/com/cmozie/classes/ZipcodeContentProvider.java
@@ -0,0 +1,13 @@
+package com.cmozie.classes;
+
+public class ZipcodeContentProvider {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
