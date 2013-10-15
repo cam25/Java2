14636bfe454831f9da3291eb9941da2cf849aad9
Cameron Mozie <camsw0rld14@gmail.com>
2013-10-15 08:16:31 -0400

    Week3 commit- added infoActivity class

---
+++ Java2Week2/src/com/cmozie/java2week2/InfoActivity.java
@@ -0,0 +1,13 @@
+package com.cmozie.java2week2;
+
+public class InfoActivity {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
